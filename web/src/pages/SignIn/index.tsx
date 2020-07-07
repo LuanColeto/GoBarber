852313c693a4b5180748ee16e83223ddf473e8df
Luan Coleto <luancoleto@outlook.com>
2020-07-06 22:15:04 -0300

    Finish the AuthContext

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,10 +1,10 @@
-import React, {useCallback, useRef, useContext} from 'react';
+import React, {useCallback, useRef} from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import * as Yup from 'yup';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 
-import {AuthContext} from '../../context/AuthContext'
+import {useAuth} from '../../hooks/AuthContext'
 
 
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -14,13 +14,17 @@ import Button from '../../components/button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string,
+  password: string
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const {signIn} = useContext(AuthContext)
+  const { signIn} = useAuth();
 
-
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -35,11 +39,17 @@ const SignIn: React.FC = () => {
         abortEarly: false,
       });
 
-      signIn()
+      signIn({
+        email: data.email,
+        password: data.password
+      })
     } catch (err) {
-      const errors = getValidationErrors(err);
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
 
-      formRef.current?.setErrors(errors);
+        formRef.current?.setErrors(errors);
+      }
+
     }
   }, [signIn],
     );
