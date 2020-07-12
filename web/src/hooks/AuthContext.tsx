a027e2971aa6c4f932b2c9e14bccab84f7b6e5d9
Luan Coleto <luancoleto@outlook.com>
2020-07-12 11:36:39 -0300

    Update Auth hook

--- web/src/hooks/AuthContext.tsx
+++ web/src/hooks/AuthContext.tsx
@@ -20,9 +20,9 @@ interface AuthContextData {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({children}) => {
-  const [data, setData] = useState<AuthState>(() => {
-    const token = localStorage.getItem('@GoBarber:token')
-    const user = localStorage.getItem('@GoBarber:user')
+    const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
       return {token, user: JSON.parse(user)}
