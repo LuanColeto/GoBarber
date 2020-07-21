55a5990181bf0619f86d8f498d7397d2ea4c37ad
Luan Coleto <luancoleto@outlook.com>
2020-07-20 21:42:49 -0300

    Add hash provider to user creation services

--- Server/src/modules/users/services/CreateUserService.ts
+++ Server/src/modules/users/services/CreateUserService.ts
@@ -1,7 +1,8 @@
-import { hash } from 'bcryptjs';
+import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import { injectable, inject } from 'tsyringe';
+
+import IHashProvider from '@modules/users/providers/hashProvider/models/IHashProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
@@ -15,7 +16,11 @@ interface IRequest {
 @injectable()
 class CreateUserService {
   constructor(
-    @inject('UsersRepository') private usersRepository: IUsersRepository,
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -25,7 +30,7 @@ class CreateUserService {
       throw new AppError('E-mail address is already used');
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = await this.usersRepository.create({
       name,
