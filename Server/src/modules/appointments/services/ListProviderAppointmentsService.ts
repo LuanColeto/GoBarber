921ccfe3d51e490d718657d7578bb64e1124e512
Luan Coleto <luancoleto@outlook.com>
2020-07-29 19:34:49 -0300

    Add: Serializing to cache

--- Server/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ Server/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
 
 interface IRequest {
@@ -42,9 +43,7 @@ class ListProviderAppointmentsService {
         year,
       });
 
-      console.log('Buscou do banco');
-
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
